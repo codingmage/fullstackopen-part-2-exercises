3f58cac0af9554969ed5207ea8b26dca27060dc2
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-09-29 23:44:12 -0300

    add exercises 2.12, 2.13, 2.14, 2.15

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import Filter from './components/Filter'
 import Form from './components/Form'
 import PeopleList from './components/PeopleList'
 import axios from 'axios'
+import personService from './services/people'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -12,10 +13,10 @@ const App = () => {
   /* const [filteredPeople, setFilteredPeople] = useState([]) */
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(numbers => {
+        setPersons(numbers)
       })
   }, [])
   
@@ -30,16 +31,49 @@ const App = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    const alreadyThere = persons.some(person => person.name === newName)
+    const alreadyThere = persons.some(person => person.name === newName && person.number === newNumber)
+
+    const samePersonNewNumber = persons.some(person => person.name === newName && person.number !== newNumber)
 
     if (alreadyThere) {
       return alert(`${newName} is already in the phonebook`)
     }
 
-    setPersons([...persons, { name: newName, number: newNumber }])
+    if (samePersonNewNumber) {
+      if (confirm(`${newName} is already in the phonebook. Replace the old number with the new one?`) == true) {
+        const selectedPerson = persons.find(p => p.name == newName) 
+        const changedPerson = {...selectedPerson, number: newNumber}
+
+        const thisPersonId = selectedPerson.id
+
+        personService
+          .update(thisPersonId, changedPerson)
+            .then(returnedPerson => {
+              setPersons(persons.map(person => person.name !== newName ? person : returnedPerson))
+              setNewName('')
+              setNewNumber('')
+            })
+        return
+      }
+    }
+
+    const personInfo = {
+      name: newName,
+      number: newNumber
+    }
+
+    personService
+      .create(personInfo)
+      .then(returnedInfo => {
+        setPersons([...persons, returnedInfo])
+        setNewName('')
+        setNewNumber('')
+      })
+
+/*     setPersons([...persons, { name: newName, number: newNumber }]) */
     /* setPersons(persons.concat({ name: newName})) */
-    setNewName('')
-    setNewNumber('')
+/*     setNewName('')
+    setNewNumber('') */
 
   }
 
@@ -47,6 +81,21 @@ const App = () => {
     setFilter(event.target.value)
 /*     const filteredList = persons.filter(person => {return person.name.toLowerCase().includes(filter.toLowerCase())})
     setFilteredPeople(filteredList) */
+  }
+
+  const handleRemoveNumber = (id, name) => {
+    if (confirm(`Delete ${name}?`) == true) {
+      const personsWithoutDeleted = persons.filter(person => person.id !== id)
+
+      personService
+        .removeNumber(id)
+         .then(setPersons(personsWithoutDeleted))
+        .catch(error => {
+          alert(
+            `Unable to delete this person.`
+          ) })
+    }
+    
   }
 
   const peopleToShow = (filter === '')
@@ -71,7 +120,7 @@ const App = () => {
 
       <h2>Numbers</h2>
 
-      <PeopleList list={peopleToShow} />
+      <PeopleList list={peopleToShow} handleDeletion={handleRemoveNumber} />
       
     </div>
   )
--- phonebook/src/components/PeopleList.jsx
+++ phonebook/src/components/PeopleList.jsx
@@ -1,7 +1,12 @@
-const PeopleList = ({list}) => {
+const PeopleList = ({list, handleDeletion}) => {
+
     return (
         <ul>
-            {list.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+            {list.map(person => 
+                <li key={person.name}>
+                    {person.name} {person.number} <button onClick={() => handleDeletion(person.id, person.name)}>delete</button>
+                </li>
+            )}
         </ul>
     )
 }
